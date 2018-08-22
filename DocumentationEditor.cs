c336b76c671e3eb7c9bb184550a29332378c64f6
Alex Zilbersher <azilby@gmail.com>
2018-08-22 02:54:10 -0400

    Add files via upload

---
+++ DocumentationEditor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+/// <summary>
+/// Adds a documentation button to a script's editor. 
+/// </summary>
+public abstract class DocumentationEditor : Editor {
+
+    protected abstract string URL { get; }
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+        DrawURL();
+	}
+
+    /// <summary>
+    /// Draws the documentation url button. 
+    /// </summary>
+    protected void DrawURL() 
+    {
+        GUILayout.Space(10);
+
+        GUILayout.Label("Documentation", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("See Documentation"))
+        {
+            Application.OpenURL(URL);
+        }
+    }
+}
+
---
+++ RevertablePrefabEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Adds a button to revert to defaults in the editor. 
+/// </summary>
+public abstract class RevertablePrefabEditor : DocumentationEditor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        DrawDefaults();
+
+        DrawURL();
+    }
+
+    protected void DrawDefaults() {
+        GUILayout.Space(10);
+
+        GUILayout.Label("Default Values", EditorStyles.boldLabel);
+        if (GUILayout.Button("Revert to Defaults"))
+        {
+            GameObject go = ((Component)target).gameObject;
+            PrefabUtility.ReconnectToLastPrefab(go);
+            PrefabUtility.RevertPrefabInstance(go);
+        }
+    }
+}
